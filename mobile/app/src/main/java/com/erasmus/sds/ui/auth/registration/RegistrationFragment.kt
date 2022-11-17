3be6d3f408ae4d509a471c839d40768039301859
Filip Grebenac <fgrebenac@gmail.com>
2022-11-17 20:58:28 +0100

    Registration and login layouts

--- mobile/app/src/main/java/com/erasmus/sds/ui/auth/registration/RegistrationFragment.kt
+++ mobile/app/src/main/java/com/erasmus/sds/ui/auth/registration/RegistrationFragment.kt
@@ -1,4 +1,23 @@
 package com.erasmus.sds.ui.auth.registration
 
-class RegistrationFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.erasmus.sds.R
+import com.erasmus.sds.databinding.FragmentRegistrationBinding
+import com.erasmus.sds.utils.onClickDebounced
+import com.erasmus.sds.utils.viewBinding
+
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
+
+    private val binding by viewBinding(FragmentRegistrationBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        binding.registerButton.onClickDebounced {
+            findNavController().navigate(RegistrationFragmentDirections.navigateToLogin())
+        }
+    }
 }
