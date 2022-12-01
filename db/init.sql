e745d2a0a886b119c1b84a50ff9282addb1a5f9f
Anteskera <ante.teskera@gmail.com>
2022-12-01 15:11:46 +0100

    added UNIQUE email constraint

--- db/init.sql
+++ db/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE app_user(
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL,
 	home_country_id UUID  NOT NULL,
 	CONSTRAINT fk_home_country_id FOREIGN KEY(home_country_id) REFERENCES country(id),
